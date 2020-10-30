6b92d68b75db1ffd25d0ff9ec0a65493f60d2fd9
Allan Commelin <commelinallan@gmail.com>
2020-10-30 18:39:22 +0100

    Try to install tailwind

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=" container mx-auto py-10 h-full bg-black">
+        <h1 class="text-red-200">Test</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

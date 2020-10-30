12859d59eeb922d0f4841eb3fcf5f1ec02ba93f7
Allan Commelin <commelinallan@gmail.com>
2020-10-30 21:40:38 +0100

    WIP Login and Resgister

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link :to="{name: 'Login'}">Login</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
+        <template v-if="!is_login">
+            | <router-link :to="{name: 'Login'}">Connexion</router-link>
+            | <router-link :to="{name: 'Register'}">S'incrire</router-link>
+        </template>
+        <template v-else>
+            | <button @click="logout">Déconnexion</button>
+        </template>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+    export default {
+        name: "Register",
+        data () {
+            return {
+                email: '',
+                password: '',
+                error: null
+            }
+        },
+        computed: {
+            is_login: () =>{
+                return localStorage.getItem('token')
+            }
+        },
+         methods: {
+             logout () {
+                 localStorage.removeItem('token')
+             }
+         }
+    }
+</script>
 
 <style lang="scss">
 #app {

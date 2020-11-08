a9975e577389de351335a28a4341d0d11d3295c1
Allan Commelin <commelinallan@gmail.com>
2020-11-08 17:42:09 +0100

    Router when we refresh we stay on the current page

--- src/api/user.js
+++ src/api/user.js
@@ -3,15 +3,15 @@ import Vue from 'vue'
 import store from '../store'
 
 export default {
-    verifyUser: () => {
+     verifyUser: async () => {
         const { sub } = jwt_decode(localStorage.getItem('token'))
-        Vue.prototype.$http.get(`http://localhost:3000/users/${sub}`)
+        await Vue.prototype.$http.get(`http://localhost:3000/users/${sub}`)
             .then(response => {
                 store.dispatch('user/loadUser', response.data)
-                return true
+                return Promise.resolve()
             }).catch((err) => {
                 if (err.response.status === 401) store.dispatch('errorUser', 'Session expirée, veuillez-vous reconnecter')
-                return false
+                return Promise.reject()
              })
     }
 }
---
+++ src/store/modules/concerts.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+
+const state = {
+
+}
+
+const getters = {}
+
+const mutations = {}
+
+const actions = {}
+
+const concerts = {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+}
+export default concerts;
